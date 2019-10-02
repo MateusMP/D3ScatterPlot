310ad6525d2e0e397b0c4ead60442f78a5c15aef
MateusMP <mateus.mp93@gmail.com>
2019-10-02 09:47:39 -0300

    Code clean up

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-
+// Scatter plot object
 const scatterPlot = {
+
+    // Color options
     colors: [
         {
             name: "Categorical",
@@ -21,7 +23,8 @@ const scatterPlot = {
         }
     ],
 
-    init: function (width, height) {
+    // Create the SVG and menu options
+    init: function (width, height, dataset) {
         this.width = width;
         this.height = height;
 
@@ -39,25 +42,25 @@ const scatterPlot = {
         this.svgG = this.svg.append("g");
 
         // Define a group for each axis
-        this.leftAxis = this.svg.append("g");
-        this.bottomAxis = this.svg.append("g");
+        this.leftAxisG = this.svg.append("g");
+        this.bottomAxisG = this.svg.append("g");
 
         // Move our axis to a proper position
-        this.leftAxis.attr("transform", `translate(${this.margin.x}, 0)`)
-        this.bottomAxis.attr("transform", `translate(0, ${this.height - this.margin.y})`);
+        this.leftAxisG.attr("transform", `translate(${this.margin.x}, 0)`)
+        this.bottomAxisG.attr("transform", `translate(0, ${this.height - this.margin.y})`);
 
         // Create axis label text element and position it
-        this.leftAxis.append("text").attr("class", "graph-labels")
+        this.leftAxisG.append("text").attr("class", "graph-labels")
             .attr("text-anchor", "middle")
             .attr("transform", `translate(${-this.margin.x + 15}, ${height / 2}) rotate(-90)`);
 
-        this.bottomAxis.append("text").attr("class", "graph-labels")
+        this.bottomAxisG.append("text").attr("class", "graph-labels")
             .attr("text-anchor", "middle")
             .attr("transform", `translate(${width / 2}, ${this.margin.y - 15})`);
 
         // Load the dataset
         const that = this;
-        d3.csv("winequality-red.csv").then(function (data) {
+        d3.csv(dataset).then(function (data) {
             that.createMenu(data);
             that.plotGraph(data);
         });
@@ -91,6 +94,18 @@ const scatterPlot = {
         this.menuY.on('change', () => this.plotGraph(data));
         this.menuColor.on('change', () => this.plotGraph(data));
     },
+    
+    getYColumn() {
+        return this.menuY.node().value;
+    },
+
+    getXColumn() {
+        return this.menuX.node().value;
+    },
+
+    getColorOption() {
+        return this.colors[this.menuColor.node().value];
+    },
 
     plotGraph: function (originalData) {
 
@@ -98,15 +113,15 @@ const scatterPlot = {
 
         // Define our X and Y scales
         const xScale = d3.scaleLinear()
-            .domain([d3.min(data, d => d.x), d3.max(data, d => d.x)])
-            .range([this.margin.x, this.width - this.margin.x]);
+            .domain([d3.min(data, d => d.x), d3.max(data, d => d.x)])   // original domain
+            .range([this.margin.x, this.width - this.margin.x]);        // map to range
 
         const yScale = d3.scaleLinear()
             .domain([d3.max(data, d => d.y), d3.min(data, d => d.y)])
             .range([this.margin.y, this.height - this.margin.y]);
 
         // Initialize the color scale
-        const color = this.colors[this.menuColor.node().value];
+        const color = this.getColorOption();
         color.init(data);
 
         // Select relevant elements and associate them with the data
@@ -116,37 +131,32 @@ const scatterPlot = {
             create => create.append("circle")
                 .attr("cx", d => xScale(d.x))
                 .attr("cy", d => yScale(d.y))
-                .attr("r", d => 3)
+                .attr("r", 3)
                 .attr("fill", d => color.transform(d.color)),
             update => update.transition().duration(1000)
                 .attr("cx", d => xScale(d.x))
                 .attr("cy", d => yScale(d.y))
-                .attr("r", d => 3)
+                .attr("r", 3)
                 .attr("fill", d => color.transform(d.color)),
             exit => null,
         );
 
         // Update axis information
         const leftAxis = d3.axisLeft(yScale);
-        this.leftAxis.transition().duration(1000).call(leftAxis);
-        this.leftAxis.select("text").text(originalData.columns[this.menuY.node().value]);
+        this.leftAxisG.transition().duration(1000).call(leftAxis);
+        this.leftAxisG.select("text").text(originalData.columns[this.getYColumn()]);
 
         const bottomAxis = d3.axisBottom(xScale);
-        this.bottomAxis.transition().duration(1000).call(bottomAxis);
-        this.bottomAxis.select("text").text(originalData.columns[this.menuX.node().value]);
+        this.bottomAxisG.transition().duration(1000).call(bottomAxis);
+        this.bottomAxisG.select("text").text(originalData.columns[this.getXColumn()]);
     },
 
     prepareData: function (data) {
-        function indexColumn(data, column) {
-            // Returns the column name given column index
-            return data.columns[column];
-        }
-
         // Get the proper columns considering the input
-        const columnX = indexColumn(data, this.menuX.node().value);
-        const columnY = indexColumn(data, this.menuY.node().value);
+        const columnX = data.columns[this.getXColumn()];
+        const columnY = data.columns[this.getYColumn()];
         // Class column assumed to be the last column
-        const classColumn = indexColumn(data, data.columns.length - 1);
+        const classColumn = data.columns[data.columns.length - 1];
 
         // Map every instance into a simpler representation with the data we need
         return data.map(d => {
@@ -155,11 +165,11 @@ const scatterPlot = {
                 y: +d[columnY],
                 color: d[classColumn]
             }
-        })
+        });
     }
 }
 
 // Init graph once the document is loaded.
 document.addEventListener("DOMContentLoaded", function (event) {
-    scatterPlot.init(600, 500);
+    scatterPlot.init(600, 500, "winequality-red.csv");
 });
